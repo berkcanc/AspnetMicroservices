f6755224021331acf12f2a6c4be7a1923e54fc44
Berkcan <berkcan_19195@hotmail.com>
2021-11-13 12:16:40 +0300

    Catalog.API Syntax Fix

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -36,7 +36,7 @@ namespace Catalog.API.Controllers
         public async Task<ActionResult<Product>> GetProductById(string id)
         {
             var product = await _repository.GetProduct(id);
-            if(product == null)
+            if (product == null)
             {
                 _logger.LogError($"Product with id: {id}, not found.");
             }
