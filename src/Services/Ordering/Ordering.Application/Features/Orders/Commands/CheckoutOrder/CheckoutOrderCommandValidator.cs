4a36728512d4d596a525d694b8bbe69394d643ad
Berkcan <berkcan_19195@hotmail.com>
2021-11-21 15:25:52 +0300

    Ordering.Application Developed

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.") // if UserName is empty
+                .NotNull() // if UserName is null
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters."); // if UserName has more than 50 characters
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero."); // if TotalPrice <= 0
+        }
+    }
+}
