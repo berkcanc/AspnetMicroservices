a1722c858407c42dc03fcc057dab866f08721116
Berkcan <berkcan_19195@hotmail.com>
2021-11-29 22:01:45 +0300

    MassTransit Implementation For Basket and Ordering.API

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -28,14 +29,25 @@ namespace Basket.API
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionStrings");
             });
 
+            // General Configuration
             // The meaning of this row => When you see IBasketRepository, convert to BasketRepository
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
 
+            // Grpc Configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new System.Uri(Configuration["GrpcSettings:DiscountUrl"]));
-
             services.AddScoped<DiscountGrpcService>();
 
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
